3b47af6636390b59d6a5a54b5175c1b2b725000b
Artorias <hjjzxcv123@gmail.com>
2024-05-16 15:27:06 +0800

    init project

---
+++ src/main/java/com/JingXiShoppingDemo/JingXiShoppingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.JingXiShoppingDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingXiShoppingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingXiShoppingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JingXiShoppingDemo/JingXiShoppingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JingXiShoppingDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingXiShoppingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
